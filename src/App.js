208e1af7ebb4a206684bd835a924abfe1edbb11a
Bohdan Benovskyi <bohdanben@tahometer.com>
2018-09-14 11:58:35 +0300

    Move ButtonCalc class to another file.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './App.css';
-
-class ButtonCalc extends React.Component {
-    render() {
-        return (
-            <button className={this.getTypeOfButton()} onClick={()=>this.props.onClick()}>
-                {this.props.value}
-            </button>
-        );
-    }
-
-    getTypeOfButton(){
-        if(this.props.value === "+" || this.props.value === "-" || this.props.value === "*" || this.props.value === "/")
-            return "arithmetic-buttons";
-        else if(this.props.value === "=")
-            return "equal-button";
-        else if(this.props.value === "clear")
-            return "clear-button";
-        else
-            return "square";
-    }
-}
+import ButtonCalc from './ButtonCalc';
 
 class FormCalculator extends React.Component{
     constructor(props){
