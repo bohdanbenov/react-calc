90df1164679447e97b088e820f0b0a612103e1a3
Bohdan Benovskyi <bohdanben@tahometer.com>
2018-09-14 15:31:14 +0300

    Add method getClass for changing style.

--- src/ButtonCalc.js
+++ src/ButtonCalc.js
@@ -1,24 +1,42 @@
+import './Button.css';
 import React from "react";
 
 class ButtonCalc extends React.Component {
     render() {
         return (
-            <button className={this.getTypeOfButton()} onClick={()=>this.props.onClick()}>
+            <button className={this.getClass(this.props.type)} onClick={()=>this.props.onClick()}>
                 {this.props.value}
             </button>
         );
     }
 
-    getTypeOfButton(){
-        if(this.props.value === "+" || this.props.value === "-" || this.props.value === "*" || this.props.value === "/")
-            return "arithmetic-buttons";
-        else if(this.props.value === "=")
-            return "equal-button";
-        else if(this.props.value === "clear")
-            return "clear-button";
-        else
-            return "square";
+    getClass(type){
+        switch (type) {
+            case 'basic':
+                return 'basic';
+                break;
+            case 'primary':
+                return 'primary';
+                break;
+            case 'success':
+                return 'success';
+                break;
+            case 'danger':
+                return 'danger';
+                break;
+        }
     }
+
+    // getTypeOfButton(){
+    //     if(this.props.value === "+" || this.props.value === "-" || this.props.value === "*" || this.props.value === "/")
+    //         return "arithmetic-buttons";
+    //     else if(this.props.value === "=")
+    //         return "equal-button";
+    //     else if(this.props.value === "clear")
+    //         return "clear-button";
+    //     else
+    //         return "square";
+    // }
 }
 
 export default ButtonCalc;
