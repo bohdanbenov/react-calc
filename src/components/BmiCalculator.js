3813bac4e612119882ae18ce49c049def162ec21
Bohdan Benovskyi <bohdanben@tahometer.com>
2018-09-17 11:24:46 +0300

    Replace two handlers by one, fix bug with immediately calculation.

--- src/components/BmiCalculator.js
+++ src/components/BmiCalculator.js
@@ -1,32 +1,20 @@
 import React from "react";
-import Button from './Button';
 import '../stylesheets/App.css'
 
 class BmiCalculator extends React.Component{
     constructor(props){
         super(props);
         this.state = {
-            weight: '',
-            height: '',
-            bmi: '',
+            weight: 0,
+            height: 0,
+            bmi: 0,
         };
     }
 
-    handleWeightField(event){
-        this.setState({weight: event.target.value})
-    }
-
-    handleHeightField(event){
-        this.setState({height: event.target.value})
-    }
-
-    handleClick(){
-        let bmi = this.state.weight/((this.state.height) * (this.state.height));
-        this.setState({bmi: bmi});
-    }
-
-    renderButton(i, style) {
-        return <Button value={i} type={style} onClick={()=>this.handleClick()} />;
+    handleBMI(event){
+        this.setState({ [event.target.name]: event.target.value }, () =>{
+            this.setState({bmi: (this.state.weight/Math.pow(this.state.height, 2))});
+        });
     }
 
     render(){
@@ -34,12 +22,12 @@ class BmiCalculator extends React.Component{
             <div className={'center'}>
                 <div className={'margin-input'}>
                     <label htmlFor='weight'>Enter your weight: </label>
-                    <input type="text" id={'weight'} onChange={this.handleWeightField.bind(this)}/>
+                    <input type="number" id={'weight'} name={'weight'} onChange={this.handleBMI.bind(this)}/>
                 </div>
 
                 <div className={'margin-input'}>
                     <label htmlFor='height'>Enter your height: </label>
-                    <input type="text" id={'height'} onChange={this.handleHeightField.bind(this)}/>
+                    <input type="number" id={'height'} name={'height'} onChange={this.handleBMI.bind(this)}/>
                 </div>
 
                 <div>
@@ -47,10 +35,6 @@ class BmiCalculator extends React.Component{
                     <h4>Your height: {this.state.height}</h4>
                     <img src="http://www.journey-fit.com/wp-content/uploads/2016/09/bmi-categories_med.jpeg" alt="bmi" />
                     <h4>Your bmi: {this.state.bmi}</h4>
-                </div>
-
-                <div>
-                    {this.renderButton('BMI', 'success')}
                 </div>
             </div>
         );
